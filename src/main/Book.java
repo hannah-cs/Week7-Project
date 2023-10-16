30da4a2a8f9bf3bad26c2b9fe97f8ca154e22512
juliabaptista <juliabaptistadeoliveira@gmail.com>
2023-10-16 15:29:44 +0200

    feat: apply the BookInfo annotation to the Book class

--- src/main/Book.java
+++ src/main/Book.java
@@ -2,17 +2,13 @@ package src.main;
 
 public class Book {
     private int id;
+    @BookInfo(title = "Title X", author = "Author X", yearPublished = 2023)
+    private String bookInfo;
     private String isbn;
-    private String title;
-    private String author;
-    private int yearPublished;
-
     public Book(int id, String isbn, String title, String author, int yearPublished) {
         this.id = id;
         this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.yearPublished = yearPublished;
+        this.bookInfo = title + " by " + author + ", " + yearPublished;
     }
 
     public int getId() {
@@ -31,27 +27,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getTitle() {
-        return title;
+    public String getBookInfo() {
+        return bookInfo;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getYearPublished() {
-        return yearPublished;
-    }
-
-    public void setYearPublished(int yearPublished) {
-        this.yearPublished = yearPublished;
+    public void setBookInfo(String bookInfo) {
+        this.bookInfo = bookInfo;
     }
 }
