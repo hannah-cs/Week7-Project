1fcd6f99f7e49675209288c7d9e2be0315c7c48a
hcs <hannahemcs@gmail.com>
2023-10-16 15:22:14 +0200

    add getters, setters

--- src/main/Member.java
+++ src/main/Member.java
@@ -5,4 +5,34 @@ public class Member {
     String name;
     @Email
     String email;
+
+    public Member(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
